2730fd0f47e0f5559e119f739b69c75331818fb4
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-05 17:37:29 +0300

    <Control Flow Lab>

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// index.js
+
+function scuberGreetingForFeet(feet) {
+  let message;
+  if (feet <= 400) {
+    message = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+
+
